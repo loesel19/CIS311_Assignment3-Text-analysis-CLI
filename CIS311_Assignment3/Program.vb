3af38daf8017df002bb09d19872bdc474ae12805
loesel19 <aloesel2@gmail.com>
2022-01-27 15:25:28 -0500

    Add project files.

---
+++ CIS311_Assignment3/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.BackgroundColor = ConsoleColor.White
+        Console.ForegroundColor = ConsoleColor.Blue
+        Console.Clear()
+        Console.WriteLine("Hello World!")
+        Console.Title() = "Word Analysis Profiler Application"
+
+    End Sub
+End Module
